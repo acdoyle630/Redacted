e7dc1759cca276122770bae8d09eaa4a3441e173
Adam <acdoyle630@gmail.com>
2017-04-19 12:11:40 -1000

    set blacklist route

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const blackList = require('./routes/blackList');
 const PORT = 3000;
 
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/blacklist', blackList);
 
 
 const server = app.listen(PORT, () =>{
