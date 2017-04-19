f723f76fdfae6d8b7ee3a161b11f7983536ddbf7
Adam <acdoyle630@gmail.com>
2017-04-19 11:59:41 -1000

    initil commit

---
+++ server.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6*/
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+const server = app.listen(PORT, () =>{
+  console.log(`server listening on port ${PORT}`);
+});
